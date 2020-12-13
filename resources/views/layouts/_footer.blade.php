4f382b3d332a151287f958573aba3816eb826f6d
heman <605184327@qq.com>
2020-12-13 23:45:14 +0000

    切割头尾部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="#">
+  <a href="#" target=_blank>
+    欢迎来到微博App!
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
