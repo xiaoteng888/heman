9b3e18affd3ca898f119d95490835b14193e3329
heman <605184327@qq.com>
2020-12-26 03:26:50 +0000

    用户中心页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
     	return view('users.create');
+    }
+
+    public function index()
+    {
+         return view('users.index');
+    }
+
+    public function show(User $user)
+    {
+         return view('users.show',['user'=>$user]);
+    }
+
+    public function store()
+    {
+         return view('users.store');
+    }
+
+    public function edit()
+    {
+         return view('users.edit');
+    }
+
+    public function update()
+    {
+         return view('users.update');
+    }
+
+    public function destroy()
+    {
+         return view('users.destroy');
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','用户列表')
+@section('content')
+<h1>用户列表</h1>
+@stop
