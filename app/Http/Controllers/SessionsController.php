e3ecf81eb4213ac449322559cd238953e70f156a
heman <605184327@qq.com>
2020-12-27 01:30:09 +0000

    记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,12 +18,19 @@ class SessionsController extends Controller
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
-       if(Auth::attempt($user)){
+       if(Auth::attempt($user,$request->has('remember'))){
            session()->flash('success','欢迎回来!');
            return redirect()->route('users.show',[Auth::user()]);
        }else{
            session()->flash('danger','账号或密码错误');
            return redirect()->back()->withInput();
        }
+    }
+
+    public function destroy()
+    {
+    	Auth::logout();
+    	session()->flash('success','您已经成功退出');
+    	return redirect('login');
     }
 }
