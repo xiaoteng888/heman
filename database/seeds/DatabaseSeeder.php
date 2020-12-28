46120c205a6e1840a8b03bf5f3e6f59143922732
heman <605184327@qq.com>
2020-12-28 08:32:01 +0000

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {  
     	Model::unguard();
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesSeeder::class);
         Model::reguard();
     }
 }
